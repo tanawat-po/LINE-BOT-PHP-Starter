c190cb2bde060209841f84242f050b440e678914
tanawat-po <tanawat.po@tnis.com>
2018-02-27 17:34:17 +0700

    edit bot

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = '4Pxy/Asa7MkjsFhy0q9XqpEaTWX3h1rnvI/O53kyHh2VbQ6Hd75C0Wm/2W/zyvIXyhuZPoVY6+KNj5NYlTfanA9P2TN68TybbvsOLG7LXVOK+s8x3M2BYPeLO2QYlEoPvpx71IWcz5zONlRjNjbDLgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
